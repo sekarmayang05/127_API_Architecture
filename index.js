1282622bdd8eb5cac5f37cb905f597e3d9a0e171
sekarmayang05 <mayangkumala05@gmail.com>
2026-07-22 15:51:59 +0700

    menmbahkan api routes

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/api', require('./routes/api'));
+
 
 async function startServer() {
     await connectDatabase();
